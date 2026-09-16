788ee1126dac988a7faae0daa54bf37d442a86fc
txjws05-debug <txjws05>
2026-09-16 10:56:16 +0800

    fix：Improve relevant things

--- backend/ai/app/main.py
+++ backend/ai/app/main.py
@@ -1,21 +1,23 @@
 from fastapi import FastAPI
 from pymilvus import settings
-
-from backend.ai.app.route.chat import  mock_stream
+from contextlib import asynccontextmanager
+from backend.ai.app.route import  chat
 from fastapi.responses import StreamingResponse
-app = FastAPI(title="Kada AI Service", version="0.1.0")
+from app.service.db import init_db
+
+@asynccontextmanager
+#确保sql表存在
+async def lifespan(app:FastAPI):
+    await init_db()
+    yield
+
+
+app = FastAPI(title="Kada AI Service", version="0.1.0",lifespan=lifespan)
 
 @app.get("/healthz")
 def healtjz():
     return {"status":"ok","service":"kada-ai","mock":settings.ai_mock}
-@app.get("/chat")
-def chat():
-    return StreamingResponse(
-        mock_stream("真的假的"),
-        media_type="text/event-steam"
-    )
 
-if __name__=="__main__":
-    import uvicorn
-    uvicorn.run(app,host="0.0.0.0",port=8000)
+#把chat.py里定义的所有接口挂到app上
+app.include_router(chat.router)
 
